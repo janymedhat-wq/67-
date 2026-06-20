78719b4523e02f5ca66bdc34569a14bfd90f2cef
Midhat <janymedhat@gmail.com>
2026-06-20 23:39:30 +0500

    Implement motor control setup and loop functions
    Added setup and loop functions for motor control.

--- test.c
+++ test.c
@@ -1,4 +1,22 @@
-#include<servo.h>
-Servo myBrushlessMotor;
+#include <Servo.h>
+Servo myBrushlessMotor; 
+const int motorPin = 9; 
+const int lowSpeed = 40; 
+const int highSpeed = 80;
+void setup() {
+  Serial.begin(9600);
+  myBrushlessMotor.attach(motorPin);
+  Serial.println("Arming ESC... Keep clear of the motor!");
+  myBrushlessMotor.write(0); 
+  delay(3000); 
+  Serial.println("ESC Armed. Starting 6-7 routine!");
+}
 
-const int motorPin=9;
+void loop() {
+  Serial.println("6...");
+  myBrushlessMotor.write(lowSpeed); 
+  delay(600); 
+  Serial.println("...7! (67!)");
+  myBrushlessMotor.write(highSpeed); 
+  delay(700); 
+}
